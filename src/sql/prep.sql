85af3c483a58d82684b580f363ff5e6d6977f175
PESchoenberg <pablo.edronkin@yahoo.com>
2019-11-28 14:13:05 -0300

    Sqlite extensions added.

--- src/sql/prep.sql
+++ src/sql/prep.sql
@@ -1,8 +1,8 @@
 UPDATE trf_pers SET Status = 'enabled' WHERE Status IS NULL;
 UPDATE trf_pers SET Context = 'center' WHERE Context IS NULL;
-UPDATE trf_pers SET Hr = 9 WHERE Hr IS NULL;
-UPDATE trf_pers SET Dayn = 21 WHERE Dayn IS NULL;
-UPDATE trf_pers SET Dayw = 'Thu' WHERE Dayw IS NULL;
+UPDATE trf_pers SET Hr = 15 WHERE Hr IS NULL;
+UPDATE trf_pers SET Dayn = 27 WHERE Dayn IS NULL;
+UPDATE trf_pers SET Dayw = 'Wed' WHERE Dayw IS NULL;
 UPDATE trf_pers SET Monthn = 11 WHERE Monthn IS NULL;
 UPDATE trf_pers SET Year = 2019 WHERE Year IS NULL;
 
