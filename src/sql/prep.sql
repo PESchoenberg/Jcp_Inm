f8896b051e8b8ca12f1d72b9409a4b0c88770fe4
PESchoenberg <pablo.edronkin@yahoo.com>
2019-12-02 15:38:21 -0300

    Data added.

--- src/sql/prep.sql
+++ src/sql/prep.sql
@@ -1,8 +1,8 @@
 UPDATE trf_pers SET Status = 'enabled' WHERE Status IS NULL;
 UPDATE trf_pers SET Context = 'center' WHERE Context IS NULL;
-UPDATE trf_pers SET Hr = 15 WHERE Hr IS NULL;
-UPDATE trf_pers SET Dayn = 27 WHERE Dayn IS NULL;
-UPDATE trf_pers SET Dayw = 'Wed' WHERE Dayw IS NULL;
-UPDATE trf_pers SET Monthn = 11 WHERE Monthn IS NULL;
+UPDATE trf_pers SET Hr = 6 WHERE Hr IS NULL;
+UPDATE trf_pers SET Dayn = 2 WHERE Dayn IS NULL;
+UPDATE trf_pers SET Dayw = 'Mon' WHERE Dayw IS NULL;
+UPDATE trf_pers SET Monthn = 12 WHERE Monthn IS NULL;
 UPDATE trf_pers SET Year = 2019 WHERE Year IS NULL;
 
